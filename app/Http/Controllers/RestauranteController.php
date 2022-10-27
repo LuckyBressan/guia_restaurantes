6de1629c9fdab37a29ffeed4a5cc870ca4990593
Lucas_Bressan <lucas.bressan@unidavi.edu.br>
2022-10-27 17:22:00 -0300

    Primeiras Alterações

---
+++ app/Http/Controllers/RestauranteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Restaurante;
+use Illuminate\Http\Request;
+
+class RestauranteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('restaurante.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Restaurante  $restaurante
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurante $restaurante)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Restaurante  $restaurante
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Restaurante $restaurante)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Restaurante  $restaurante
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Restaurante $restaurante)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Restaurante  $restaurante
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Restaurante $restaurante)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestauranteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+
+Route::resource('restaurantes',RestauranteController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
