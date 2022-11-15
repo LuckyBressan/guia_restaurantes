a52312c49024ffed52f91b91e59b544802eae8dc
Lucas Bressan <lucas.a.dos.s.bressan@gmail.com>
2022-11-15 14:36:41 -0300

    finalizações

--- app/Http/Controllers/CardapioBaresController.php
+++ app/Http/Controllers/CardapioBaresController.php
@@ -24,7 +24,7 @@ class CardapioBaresController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create($id)
+    public function create()
     {
         if((Auth::check()) && (Auth::user()->isAdmin())) {
             $bares = Bares::all();
